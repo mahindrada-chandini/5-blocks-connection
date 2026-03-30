f1e5fcfa12e2a85106b0dbcc26d5fe4f00991902
mahindrada-chandini <chandinimahindrada@gmail.com>
2026-03-30 22:05:08 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,494 +1,129 @@
 'use strict';
 
-/*!
- * Pug
- * Copyright(c) 2010 TJ Holowaychuk <tj@vision-media.ca>
- * MIT Licensed
- */
+var GetIntrinsic = require('get-intrinsic');
+var callBound = require('call-bind/callBound');
+var inspect = require('object-inspect');
 
-/**
- * Module dependencies.
- */
+var $TypeError = require('es-errors/type');
+var $WeakMap = GetIntrinsic('%WeakMap%', true);
+var $Map = GetIntrinsic('%Map%', true);
 
-var fs = require('fs');
-var path = require('path');
-var lex = require('pug-lexer');
-var stripComments = require('pug-strip-comments');
-var parse = require('pug-parser');
-var load = require('pug-load');
-var filters = require('pug-filters');
-var link = require('pug-linker');
-var generateCode = require('pug-code-gen');
-var runtime = require('pug-runtime');
-var runtimeWrap = require('pug-runtime/wrap');
+var $weakMapGet = callBound('WeakMap.prototype.get', true);
+var $weakMapSet = callBound('WeakMap.prototype.set', true);
+var $weakMapHas = callBound('WeakMap.prototype.has', true);
+var $mapGet = callBound('Map.prototype.get', true);
+var $mapSet = callBound('Map.prototype.set', true);
+var $mapHas = callBound('Map.prototype.has', true);
 
-/**
- * Name for detection
- */
-
-exports.name = 'Pug';
-
-/**
- * Pug runtime helpers.
- */
-
-exports.runtime = runtime;
-
-/**
- * Template function cache.
- */
-
-exports.cache = {};
-
-function applyPlugins(value, options, plugins, name) {
-  return plugins.reduce(function(value, plugin) {
-    return plugin[name] ? plugin[name](value, options) : value;
-  }, value);
-}
-
-function findReplacementFunc(plugins, name) {
-  var eligiblePlugins = plugins.filter(function(plugin) {
-    return plugin[name];
-  });
-
-  if (eligiblePlugins.length > 1) {
-    throw new Error('Two or more plugins all implement ' + name + ' method.');
-  } else if (eligiblePlugins.length) {
-    return eligiblePlugins[0][name].bind(eligiblePlugins[0]);
-  }
-  return null;
-}
-
-/**
- * Object for global custom filters.  Note that you can also just pass a `filters`
- * option to any other method.
- */
-exports.filters = {};
-
-/**
- * Compile the given `str` of pug and return a function body.
- *
- * @param {String} str
- * @param {Object} options
- * @return {Object}
- * @api private
- */
-
-function compileBody(str, options) {
-  var debug_sources = {};
-  debug_sources[options.filename] = str;
-  var dependencies = [];
-  var plugins = options.plugins || [];
-  var ast = load.string(str, {
-    filename: options.filename,
-    basedir: options.basedir,
-    lex: function(str, options) {
-      var lexOptions = {};
-      Object.keys(options).forEach(function(key) {
-        lexOptions[key] = options[key];
-      });
-      lexOptions.plugins = plugins
-        .filter(function(plugin) {
-          return !!plugin.lex;
-        })
-        .map(function(plugin) {
-          return plugin.lex;
-        });
-      var contents = applyPlugins(
-        str,
-        {filename: options.filename},
-        plugins,
-        'preLex'
-      );
-      return applyPlugins(
-        lex(contents, lexOptions),
-        options,
-        plugins,
-        'postLex'
-      );
-    },
-    parse: function(tokens, options) {
-      tokens = tokens.map(function(token) {
-        if (token.type === 'path' && path.extname(token.val) === '') {
-          return {
-            type: 'path',
-            loc: token.loc,
-            val: token.val + '.pug',
-          };
-        }
-        return token;
-      });
-      tokens = stripComments(tokens, options);
-      tokens = applyPlugins(tokens, options, plugins, 'preParse');
-      var parseOptions = {};
-      Object.keys(options).forEach(function(key) {
-        parseOptions[key] = options[key];
-      });
-      parseOptions.plugins = plugins
-        .filter(function(plugin) {
-          return !!plugin.parse;
-        })
-        .map(function(plugin) {
-          return plugin.parse;
-        });
-
-      return applyPlugins(
-        applyPlugins(
-          parse(tokens, parseOptions),
-          options,
-          plugins,
-          'postParse'
-        ),
-        options,
-        plugins,
-        'preLoad'
-      );
-    },
-    resolve: function(filename, source, loadOptions) {
-      var replacementFunc = findReplacementFunc(plugins, 'resolve');
-      if (replacementFunc) {
-        return replacementFunc(filename, source, options);
-      }
-
-      return load.resolve(filename, source, loadOptions);
-    },
-    read: function(filename, loadOptions) {
-      dependencies.push(filename);
-
-      var contents;
-
-      var replacementFunc = findReplacementFunc(plugins, 'read');
-      if (replacementFunc) {
-        contents = replacementFunc(filename, options);
-      } else {
-        contents = load.read(filename, loadOptions);
-      }
-
-      debug_sources[filename] = Buffer.isBuffer(contents)
-        ? contents.toString('utf8')
-        : contents;
-      return contents;
-    },
-  });
-  ast = applyPlugins(ast, options, plugins, 'postLoad');
-  ast = applyPlugins(ast, options, plugins, 'preFilters');
-
-  var filtersSet = {};
-  Object.keys(exports.filters).forEach(function(key) {
-    filtersSet[key] = exports.filters[key];
-  });
-  if (options.filters) {
-    Object.keys(options.filters).forEach(function(key) {
-      filtersSet[key] = options.filters[key];
-    });
-  }
-  ast = filters.handleFilters(
-    ast,
-    filtersSet,
-    options.filterOptions,
-    options.filterAliases
-  );
-
-  ast = applyPlugins(ast, options, plugins, 'postFilters');
-  ast = applyPlugins(ast, options, plugins, 'preLink');
-  ast = link(ast);
-  ast = applyPlugins(ast, options, plugins, 'postLink');
-
-  // Compile
-  ast = applyPlugins(ast, options, plugins, 'preCodeGen');
-  var js = (findReplacementFunc(plugins, 'generateCode') || generateCode)(ast, {
-    pretty: options.pretty,
-    compileDebug: options.compileDebug,
-    doctype: options.doctype,
-    inlineRuntimeFunctions: options.inlineRuntimeFunctions,
-    globals: options.globals,
-    self: options.self,
-    includeSources: options.includeSources ? debug_sources : false,
-    templateName: options.templateName,
-  });
-  js = applyPlugins(js, options, plugins, 'postCodeGen');
-
-  // Debug compiler
-  if (options.debug) {
-    console.error(
-      '\nCompiled Function:\n\n\u001b[90m%s\u001b[0m',
-      js.replace(/^/gm, '  ')
-    );
-  }
-
-  return {body: js, dependencies: dependencies};
-}
-
-/**
- * Get the template from a string or a file, either compiled on-the-fly or
- * read from cache (if enabled), and cache the template if needed.
- *
- * If `str` is not set, the file specified in `options.filename` will be read.
- *
- * If `options.cache` is true, this function reads the file from
- * `options.filename` so it must be set prior to calling this function.
- *
- * @param {Object} options
- * @param {String=} str
- * @return {Function}
- * @api private
- */
-function handleTemplateCache(options, str) {
-  var key = options.filename;
-  if (options.cache && exports.cache[key]) {
-    return exports.cache[key];
-  } else {
-    if (str === undefined) str = fs.readFileSync(options.filename, 'utf8');
-    var templ = exports.compile(str, options);
-    if (options.cache) exports.cache[key] = templ;
-    return templ;
-  }
-}
-
-/**
- * Compile a `Function` representation of the given pug `str`.
- *
- * Options:
- *
- *   - `compileDebug` when `false` debugging code is stripped from the compiled
-       template, when it is explicitly `true`, the source code is included in
-       the compiled template for better accuracy.
- *   - `filename` used to improve errors when `compileDebug` is not `false` and to resolve imports/extends
- *
- * @param {String} str
- * @param {Options} options
- * @return {Function}
- * @api public
- */
-
-exports.compile = function(str, options) {
-  var options = options || {};
-
-  str = String(str);
-
-  var parsed = compileBody(str, {
-    compileDebug: options.compileDebug !== false,
-    filename: options.filename,
-    basedir: options.basedir,
-    pretty: options.pretty,
-    doctype: options.doctype,
-    inlineRuntimeFunctions: options.inlineRuntimeFunctions,
-    globals: options.globals,
-    self: options.self,
-    includeSources: options.compileDebug === true,
-    debug: options.debug,
-    templateName: 'template',
-    filters: options.filters,
-    filterOptions: options.filterOptions,
-    filterAliases: options.filterAliases,
-    plugins: options.plugins,
-  });
-
-  var res = options.inlineRuntimeFunctions
-    ? new Function('', parsed.body + ';return template;')()
-    : runtimeWrap(parsed.body);
-
-  res.dependencies = parsed.dependencies;
-
-  return res;
+/*
+* This function traverses the list returning the node corresponding to the given key.
+*
+* That node is also moved to the head of the list, so that if it's accessed again we don't need to traverse the whole list. By doing so, all the recently used nodes can be accessed relatively quickly.
+*/
+/** @type {import('.').listGetNode} */
+var listGetNode = function (list, key) { // eslint-disable-line consistent-return
+	/** @type {typeof list | NonNullable<(typeof list)['next']>} */
+	var prev = list;
+	/** @type {(typeof list)['next']} */
+	var curr;
+	for (; (curr = prev.next) !== null; prev = curr) {
+		if (curr.key === key) {
+			prev.next = curr.next;
+			// eslint-disable-next-line no-extra-parens
+			curr.next = /** @type {NonNullable<typeof list.next>} */ (list.next);
+			list.next = curr; // eslint-disable-line no-param-reassign
+			return curr;
+		}
+	}
 };
 
-/**
- * Compile a JavaScript source representation of the given pug `str`.
- *
- * Options:
- *
- *   - `compileDebug` When it is `true`, the source code is included in
- *     the compiled template for better error messages.
- *   - `filename` used to improve errors when `compileDebug` is not `true` and to resolve imports/extends
- *   - `name` the name of the resulting function (defaults to "template")
- *   - `module` when it is explicitly `true`, the source code include export module syntax
- *
- * @param {String} str
- * @param {Options} options
- * @return {Object}
- * @api public
- */
-
-exports.compileClientWithDependenciesTracked = function(str, options) {
-  var options = options || {};
-
-  str = String(str);
-  var parsed = compileBody(str, {
-    compileDebug: options.compileDebug,
-    filename: options.filename,
-    basedir: options.basedir,
-    pretty: options.pretty,
-    doctype: options.doctype,
-    inlineRuntimeFunctions: options.inlineRuntimeFunctions !== false,
-    globals: options.globals,
-    self: options.self,
-    includeSources: options.compileDebug,
-    debug: options.debug,
-    templateName: options.name || 'template',
-    filters: options.filters,
-    filterOptions: options.filterOptions,
-    filterAliases: options.filterAliases,
-    plugins: options.plugins,
-  });
-
-  var body = parsed.body;
-
-  if (options.module) {
-    if (options.inlineRuntimeFunctions === false) {
-      body = 'var pug = require("pug-runtime");' + body;
-    }
-    body += ' module.exports = ' + (options.name || 'template') + ';';
-  }
-
-  return {body: body, dependencies: parsed.dependencies};
+/** @type {import('.').listGet} */
+var listGet = function (objects, key) {
+	var node = listGetNode(objects, key);
+	return node && node.value;
+};
+/** @type {import('.').listSet} */
+var listSet = function (objects, key, value) {
+	var node = listGetNode(objects, key);
+	if (node) {
+		node.value = value;
+	} else {
+		// Prepend the new node to the beginning of the list
+		objects.next = /** @type {import('.').ListNode<typeof value>} */ ({ // eslint-disable-line no-param-reassign, no-extra-parens
+			key: key,
+			next: objects.next,
+			value: value
+		});
+	}
+};
+/** @type {import('.').listHas} */
+var listHas = function (objects, key) {
+	return !!listGetNode(objects, key);
 };
 
-/**
- * Compile a JavaScript source representation of the given pug `str`.
- *
- * Options:
- *
- *   - `compileDebug` When it is `true`, the source code is included in
- *     the compiled template for better error messages.
- *   - `filename` used to improve errors when `compileDebug` is not `true` and to resolve imports/extends
- *   - `name` the name of the resulting function (defaults to "template")
- *
- * @param {String} str
- * @param {Options} options
- * @return {String}
- * @api public
- */
-exports.compileClient = function(str, options) {
-  return exports.compileClientWithDependenciesTracked(str, options).body;
-};
+/** @type {import('.')} */
+module.exports = function getSideChannel() {
+	/** @type {WeakMap<object, unknown>} */ var $wm;
+	/** @type {Map<object, unknown>} */ var $m;
+	/** @type {import('.').RootNode<unknown>} */ var $o;
 
-/**
- * Compile a `Function` representation of the given pug file.
- *
- * Options:
- *
- *   - `compileDebug` when `false` debugging code is stripped from the compiled
-       template, when it is explicitly `true`, the source code is included in
-       the compiled template for better accuracy.
- *
- * @param {String} path
- * @param {Options} options
- * @return {Function}
- * @api public
- */
-exports.compileFile = function(path, options) {
-  options = options || {};
-  options.filename = path;
-  return handleTemplateCache(options);
-};
-
-/**
- * Render the given `str` of pug.
- *
- * Options:
- *
- *   - `cache` enable template caching
- *   - `filename` filename required for `include` / `extends` and caching
- *
- * @param {String} str
- * @param {Object|Function} options or fn
- * @param {Function|undefined} fn
- * @returns {String}
- * @api public
- */
-
-exports.render = function(str, options, fn) {
-  // support callback API
-  if ('function' == typeof options) {
-    (fn = options), (options = undefined);
-  }
-  if (typeof fn === 'function') {
-    var res;
-    try {
-      res = exports.render(str, options);
-    } catch (ex) {
-      return fn(ex);
-    }
-    return fn(null, res);
-  }
-
-  options = options || {};
-
-  // cache requires .filename
-  if (options.cache && !options.filename) {
-    throw new Error('the "filename" option is required for caching');
-  }
-
-  return handleTemplateCache(options, str)(options);
-};
-
-/**
- * Render a Pug file at the given `path`.
- *
- * @param {String} path
- * @param {Object|Function} options or callback
- * @param {Function|undefined} fn
- * @returns {String}
- * @api public
- */
-
-exports.renderFile = function(path, options, fn) {
-  // support callback API
-  if ('function' == typeof options) {
-    (fn = options), (options = undefined);
-  }
-  if (typeof fn === 'function') {
-    var res;
-    try {
-      res = exports.renderFile(path, options);
-    } catch (ex) {
-      return fn(ex);
-    }
-    return fn(null, res);
-  }
-
-  options = options || {};
-
-  options.filename = path;
-  return handleTemplateCache(options)(options);
-};
-
-/**
- * Compile a Pug file at the given `path` for use on the client.
- *
- * @param {String} path
- * @param {Object} options
- * @returns {String}
- * @api public
- */
-
-exports.compileFileClient = function(path, options) {
-  var key = path + ':client';
-  options = options || {};
-
-  options.filename = path;
-
-  if (options.cache && exports.cache[key]) {
-    return exports.cache[key];
-  }
-
-  var str = fs.readFileSync(options.filename, 'utf8');
-  var out = exports.compileClient(str, options);
-  if (options.cache) exports.cache[key] = out;
-  return out;
-};
-
-/**
- * Express support.
- */
-
-exports.__express = function(path, options, fn) {
-  if (
-    options.compileDebug == undefined &&
-    process.env.NODE_ENV === 'production'
-  ) {
-    options.compileDebug = false;
-  }
-  exports.renderFile(path, options, fn);
+	/** @type {import('.').Channel} */
+	var channel = {
+		assert: function (key) {
+			if (!channel.has(key)) {
+				throw new $TypeError('Side channel does not contain ' + inspect(key));
+			}
+		},
+		get: function (key) { // eslint-disable-line consistent-return
+			if ($WeakMap && key && (typeof key === 'object' || typeof key === 'function')) {
+				if ($wm) {
+					return $weakMapGet($wm, key);
+				}
+			} else if ($Map) {
+				if ($m) {
+					return $mapGet($m, key);
+				}
+			} else {
+				if ($o) { // eslint-disable-line no-lonely-if
+					return listGet($o, key);
+				}
+			}
+		},
+		has: function (key) {
+			if ($WeakMap && key && (typeof key === 'object' || typeof key === 'function')) {
+				if ($wm) {
+					return $weakMapHas($wm, key);
+				}
+			} else if ($Map) {
+				if ($m) {
+					return $mapHas($m, key);
+				}
+			} else {
+				if ($o) { // eslint-disable-line no-lonely-if
+					return listHas($o, key);
+				}
+			}
+			return false;
+		},
+		set: function (key, value) {
+			if ($WeakMap && key && (typeof key === 'object' || typeof key === 'function')) {
+				if (!$wm) {
+					$wm = new $WeakMap();
+				}
+				$weakMapSet($wm, key, value);
+			} else if ($Map) {
+				if (!$m) {
+					$m = new $Map();
+				}
+				$mapSet($m, key, value);
+			} else {
+				if (!$o) {
+					// Initialize the linked list as an empty node, so that we don't have to special-case handling of the first node: we can always refer to it as (previous node).next, instead of something like (list).head
+					$o = { key: {}, next: null };
+				}
+				listSet($o, key, value);
+			}
+		}
+	};
+	return channel;
 };
--- test/index.js
+++ test/index.js
@@ -1,80 +1,83 @@
 'use strict';
 
-var callBind = require('../');
-var bind = require('function-bind');
-var gOPD = require('gopd');
-var hasStrictMode = require('has-strict-mode')();
-var forEach = require('for-each');
-var inspect = require('object-inspect');
-var v = require('es-value-fixtures');
-
 var test = require('tape');
 
-/*
- * older engines have length nonconfigurable
- * in io.js v3, it is configurable except on bound functions, hence the .bind()
- */
-var functionsHaveConfigurableLengths = !!(
-	gOPD
-	&& Object.getOwnPropertyDescriptor
-	&& Object.getOwnPropertyDescriptor(bind.call(function () {}), 'length').configurable
-);
+var getSideChannel = require('../');
 
-test('callBind', function (t) {
-	forEach(v.nonFunctions, function (nonFunction) {
-		t['throws'](
-			function () { callBind(nonFunction); },
-			TypeError,
-			inspect(nonFunction) + ' is not a function'
-		);
-	});
+test('export', function (t) {
+	t.equal(typeof getSideChannel, 'function', 'is a function');
+	t.equal(getSideChannel.length, 0, 'takes no arguments');
 
-	var sentinel = { sentinel: true };
-	var func = function (a, b) {
-		// eslint-disable-next-line no-invalid-this
-		return [!hasStrictMode && this === global ? undefined : this, a, b];
-	};
-	t.equal(func.length, 2, 'original function length is 2');
-	t.deepEqual(func(), [undefined, undefined, undefined], 'unbound func with too few args');
-	t.deepEqual(func(1, 2), [undefined, 1, 2], 'unbound func with right args');
-	t.deepEqual(func(1, 2, 3), [undefined, 1, 2], 'unbound func with too many args');
+	var channel = getSideChannel();
+	t.ok(channel, 'is truthy');
+	t.equal(typeof channel, 'object', 'is an object');
 
-	var bound = callBind(func);
-	t.equal(bound.length, func.length + 1, 'function length is preserved', { skip: !functionsHaveConfigurableLengths });
-	t.deepEqual(bound(), [undefined, undefined, undefined], 'bound func with too few args');
-	t.deepEqual(bound(1, 2), [hasStrictMode ? 1 : Object(1), 2, undefined], 'bound func with right args');
-	t.deepEqual(bound(1, 2, 3), [hasStrictMode ? 1 : Object(1), 2, 3], 'bound func with too many args');
+	t.end();
+});
 
-	var boundR = callBind(func, sentinel);
-	t.equal(boundR.length, func.length, 'function length is preserved', { skip: !functionsHaveConfigurableLengths });
-	t.deepEqual(boundR(), [sentinel, undefined, undefined], 'bound func with receiver, with too few args');
-	t.deepEqual(boundR(1, 2), [sentinel, 1, 2], 'bound func with receiver, with right args');
-	t.deepEqual(boundR(1, 2, 3), [sentinel, 1, 2], 'bound func with receiver, with too many args');
+test('assert', function (t) {
+	var channel = getSideChannel();
+	t['throws'](
+		function () { channel.assert({}); },
+		TypeError,
+		'nonexistent value throws'
+	);
 
-	var boundArg = callBind(func, sentinel, 1);
-	t.equal(boundArg.length, func.length - 1, 'function length is preserved', { skip: !functionsHaveConfigurableLengths });
-	t.deepEqual(boundArg(), [sentinel, 1, undefined], 'bound func with receiver and arg, with too few args');
-	t.deepEqual(boundArg(2), [sentinel, 1, 2], 'bound func with receiver and arg, with right arg');
-	t.deepEqual(boundArg(2, 3), [sentinel, 1, 2], 'bound func with receiver and arg, with too many args');
+	var o = {};
+	channel.set(o, 'data');
+	t.doesNotThrow(function () { channel.assert(o); }, 'existent value noops');
 
-	t.test('callBind.apply', function (st) {
-		var aBound = callBind.apply(func);
-		st.deepEqual(aBound(sentinel), [sentinel, undefined, undefined], 'apply-bound func with no args');
-		st.deepEqual(aBound(sentinel, [1], 4), [sentinel, 1, undefined], 'apply-bound func with too few args');
-		st.deepEqual(aBound(sentinel, [1, 2], 4), [sentinel, 1, 2], 'apply-bound func with right args');
+	t.end();
+});
 
-		var aBoundArg = callBind.apply(func);
-		st.deepEqual(aBoundArg(sentinel, [1, 2, 3], 4), [sentinel, 1, 2], 'apply-bound func with too many args');
-		st.deepEqual(aBoundArg(sentinel, [1, 2], 4), [sentinel, 1, 2], 'apply-bound func with right args');
-		st.deepEqual(aBoundArg(sentinel, [1], 4), [sentinel, 1, undefined], 'apply-bound func with too few args');
+test('has', function (t) {
+	var channel = getSideChannel();
+	/** @type {unknown[]} */ var o = [];
 
-		var aBoundR = callBind.apply(func, sentinel);
-		st.deepEqual(aBoundR([1, 2, 3], 4), [sentinel, 1, 2], 'apply-bound func with receiver and too many args');
-		st.deepEqual(aBoundR([1, 2], 4), [sentinel, 1, 2], 'apply-bound func with receiver and right args');
-		st.deepEqual(aBoundR([1], 4), [sentinel, 1, undefined], 'apply-bound func with receiver and too few args');
+	t.equal(channel.has(o), false, 'nonexistent value yields false');
 
-		st.end();
-	});
+	channel.set(o, 'foo');
+	t.equal(channel.has(o), true, 'existent value yields true');
+
+	t.equal(channel.has('abc'), false, 'non object value non existent yields false');
+
+	channel.set('abc', 'foo');
+	t.equal(channel.has('abc'), true, 'non object value that exists yields true');
+
+	t.end();
+});
+
+test('get', function (t) {
+	var channel = getSideChannel();
+	var o = {};
+	t.equal(channel.get(o), undefined, 'nonexistent value yields undefined');
+
+	var data = {};
+	channel.set(o, data);
+	t.equal(channel.get(o), data, '"get" yields data set by "set"');
+
+	t.end();
+});
+
+test('set', function (t) {
+	var channel = getSideChannel();
+	var o = function () {};
+	t.equal(channel.get(o), undefined, 'value not set');
+
+	channel.set(o, 42);
+	t.equal(channel.get(o), 42, 'value was set');
+
+	channel.set(o, Infinity);
+	t.equal(channel.get(o), Infinity, 'value was set again');
+
+	var o2 = {};
+	channel.set(o2, 17);
+	t.equal(channel.get(o), Infinity, 'o is not modified');
+	t.equal(channel.get(o2), 17, 'o2 is set');
+
+	channel.set(o, 14);
+	t.equal(channel.get(o), 14, 'o is modified');
+	t.equal(channel.get(o2), 17, 'o2 is not modified');
 
 	t.end();
 });
